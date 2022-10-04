55cf3444a5e2a34fc9a14e59a414b9806aa1726b
INKSec <michael.burkhardt89@web.de>
2022-10-04 10:31:24 +0200

    Added saving function for additional cameras

--- KameraAppVierKameras/KameraAppVierKameras.cpp
+++ KameraAppVierKameras/KameraAppVierKameras.cpp
@@ -1,6 +1,8 @@
 /*
 # Imporant
-# only quit program with ESC 
+# only quit program with ESC!
+# Needs Opencv
+# Change counter manually if you make a break
 */
 
 #include <iostream>
@@ -20,15 +22,19 @@ int main()
     int n = 5; // Seconds between pictures
     int milliSeconds = n * 1000; 
 
-    Mat myImage, myImage1, myImage2;
+    Mat myImage, myImage1, myImage2, myImage3;
+
     namedWindow("Camera 1"); //Declaring a matrix to load the frames
     namedWindow("Camera 2");
-    namedWindow("Camera 3");
+    //namedWindow("Camera 3");
+
     VideoCapture cap(0); // opens camera at port 0
     VideoCapture cap1(1);
-    VideoCapture cap2(2);
-    if (!cap.isOpened()) {
-        cout << "No Video" << endl;
+    //VideoCapture cap2(2);
+
+
+    if (!cap.isOpened() || !cap1.isOpened()) { // Maybe add cap2 and cap3
+        cout << "One Camera is not working" << endl;
         system("pause");
         return -1;
     }
@@ -36,19 +42,25 @@ int main()
     while (true) {
         cap >> myImage;
         cap1 >> myImage1;
-        cap2 >> myImage2;
+        //cap2 >> myImage2;
 
         if (myImage.empty()) { 
             break; // breaks loop if video frame is empty
         }
+
         imshow("Camera 1", myImage); //Showing the video
         imshow("Camera 2", myImage1);
-        imshow("Camera 3", myImage2);
+        //imshow("Camera 3", myImage2);
+        
         // Saves image every n seconds
         if (time(0) - start == n) {
-            imwrite("/Users/micha/Desktop/pictures/" + to_string(counter) + ".png", myImage); // Saves an image
-            start = start + n;
+            imwrite("/Users/micha/Desktop/pictures/" + to_string(counter) + ".png", myImage);// Saves an image
             counter++;
+            imwrite("/Users/micha/Desktop/pictures/" + to_string(counter) + ".png", myImage1);
+            counter++;
+            //imwrite("/Users/micha/Desktop/pictures/" + to_string(counter) + ".png", myImage2);
+            start = start + n;
+            //counter++;
         }
         
         char c = (char)waitKey(25); //Allowing 25 milliseconds frame processing time and initiating break condition
@@ -57,7 +69,11 @@ int main()
         
     } 
     
+    
     cap.release(); // release buffer memory
+    cap1.release();
+    //cap2.release();
+    //cap3.release();
     return 0;
 
 
