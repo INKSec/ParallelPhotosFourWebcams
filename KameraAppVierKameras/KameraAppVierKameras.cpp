fd216887eed540e51a3002e21376df9774d08fb4
INKSec <michael.burkhardt89@web.de>
2022-10-04 10:06:40 +0200

    Added two more cameras

--- KameraAppVierKameras/KameraAppVierKameras.cpp
+++ KameraAppVierKameras/KameraAppVierKameras.cpp
@@ -20,9 +20,13 @@ int main()
     int n = 5; // Seconds between pictures
     int milliSeconds = n * 1000; 
 
-    Mat myImage;
+    Mat myImage, myImage1, myImage2;
     namedWindow("Camera 1"); //Declaring a matrix to load the frames
+    namedWindow("Camera 2");
+    namedWindow("Camera 3");
     VideoCapture cap(0); // opens camera at port 0
+    VideoCapture cap1(1);
+    VideoCapture cap2(2);
     if (!cap.isOpened()) {
         cout << "No Video" << endl;
         system("pause");
@@ -31,15 +35,18 @@ int main()
 
     while (true) {
         cap >> myImage;
+        cap1 >> myImage1;
+        cap2 >> myImage2;
 
         if (myImage.empty()) { 
             break; // breaks loop if video frame is empty
         }
         imshow("Camera 1", myImage); //Showing the video
-
+        imshow("Camera 2", myImage1);
+        imshow("Camera 3", myImage2);
         // Saves image every n seconds
         if (time(0) - start == n) {
-            imwrite("/Users/Micha/Desktop/pictures/" + to_string(counter) + ".png", myImage); // Saves an image
+            imwrite("/Users/micha/Desktop/pictures/" + to_string(counter) + ".png", myImage); // Saves an image
             start = start + n;
             counter++;
         }
