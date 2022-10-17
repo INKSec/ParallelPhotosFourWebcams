b0fe4be5b49c22eb967b061064f11462108f23b9
Micha <michael.burkhardt@student.fh-kiel.de>
2022-10-17 12:44:15 +0200

    Added possibility to change camera resolution

--- KameraAppVierKameras/KameraAppVierKameras.cpp
+++ KameraAppVierKameras/KameraAppVierKameras.cpp
@@ -10,32 +10,44 @@
 #include <time.h>
 #include <windows.h>
 
+
+
+
 using namespace std;
 using namespace cv;
 
-long counterR = 60000, counterL = 60000; // counter to name pictures
+
 
 
 int main()
 {
-    
+    long counterR = 60000, counterL = 60000; // counter to name pictures
+    const int width{ 3072 }, height{ 2304 }; // Camera resolution
+    const int secondsBetweenPictures = 5;
+    //const int milliseconds = secondsBetweenPictures * 1000;
     time_t start, end;
     start = time(0);
-    int n = 5; // Seconds between pictures
-    int milliSeconds = n * 1000; 
-
     Mat myImage, myImage1, myImage2, myImage3;
 
+    
+
+
+
     namedWindow("Camera 1"); //Declaring a matrix to load the frames
-    namedWindow("Camera 2");
+    //namedWindow("Camera 2");
     //namedWindow("Camera 3");
+    //namedWindow("Camera 4");
 
     VideoCapture cap(0); // opens camera at port 0
-    VideoCapture cap1(3);
+    //VideoCapture cap1(1);
     //VideoCapture cap2(2);
+    //VideoCapture cap2(3);
+
+    cap.set(CAP_PROP_FRAME_WIDTH, width); // sets resolution width
+    cap.set(CAP_PROP_FRAME_HEIGHT, height); // sets resolution height
 
 
-    if (!cap.isOpened() || !cap1.isOpened()) { // Maybe add cap2 and cap3
+    if (!cap.isOpened()) { // Maybe add cap2 and cap3
         cout << "One Camera is not working" << endl;
         system("pause");
         return -1;
@@ -43,27 +55,30 @@ int main()
 
     while (true) {
         cap >> myImage;
-        cap1 >> myImage1;
+        //cap1 >> myImage1;
         //cap2 >> myImage2;
+        //cap3 >> myImage3;
 
         if (myImage.empty()) { 
-            break; // breaks loop if video frame is empty
+            break; 
         }
 
-        imshow("Camera 1", myImage); //Showing the video
-        imshow("Camera 2", myImage1);
+        imshow("Camera 1", myImage); //Showing the video. Is not needed to take pictures
+        //imshow("Camera 2", myImage1);
         //imshow("Camera 3", myImage2);
+        //imshow("Camera 4", myImage3);
         
         // Saves image every n seconds
-        if (time(0) - start == n) {
+        if (time(0) - start == secondsBetweenPictures) {
             Beep(1500, 100);
-            imwrite("/Users/micha/Desktop/picturesR/" + to_string(counterL) + "R.png", myImage);// Saves an image
+            imwrite("/Users/Micha/Desktop/picturesL/" + to_string(counterL) + "R.png", myImage);// Saves an image
             counterR++;
-            imwrite("/Users/micha/Desktop/picturesL/" + to_string(counterR) + "L.png", myImage1);
+            //imwrite("/Users/Micha/Desktop/picturesL/" + to_string(counterR) + "L.png", myImage1);
             counterL++;
             //imwrite("/Users/micha/Desktop/pictures/" + to_string(counter) + ".png", myImage2);
-            start = start + n;
-            //counter++;
+            //imwrite("/Users/micha/Desktop/pictures/" + to_string(counter) + ".png", myImage3);
+            start = start + secondsBetweenPictures;
+            
         }
         
         char c = (char)waitKey(25); //Allowing 25 milliseconds frame processing time and initiating break condition
@@ -74,7 +89,7 @@ int main()
     
     
     cap.release(); // release buffer memory
-    cap1.release();
+    //cap1.release();
     //cap2.release();
     //cap3.release();
     return 0;
